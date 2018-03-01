39d4128de5babdfd6f346bc313d46bef1108938e
Andrii Kryvko <andrii.kryvko@zalando.de>
2018-03-01 19:14:17 +0100

    2nd

--- src/main/java/org/hedgebits/hashcode/Application.java
+++ src/main/java/org/hedgebits/hashcode/Application.java
@@ -1,8 +1,17 @@
 package org.hedgebits.hashcode;
 
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        final Scanner scanner = new Scanner(Resources.getResource("./sample1.txt").openStream());
+        final int rows = scanner.nextInt();
+
 
     }
 
